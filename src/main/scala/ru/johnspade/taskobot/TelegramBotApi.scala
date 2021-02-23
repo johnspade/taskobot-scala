a4b0e4371a08b21c5915a5eaa51c69959f61d0fc
johnspade <ivan@ilopatin.ru>
2021-02-23 22:46:22 +0300

    Get rid of ExecutionContexts.global

--- src/main/scala/ru/johnspade/taskobot/TelegramBotApi.scala
+++ src/main/scala/ru/johnspade/taskobot/TelegramBotApi.scala
@@ -1,18 +1,20 @@
 package ru.johnspade.taskobot
 
+import cats.effect.ConcurrentEffect
 import org.http4s.client.blaze.BlazeClientBuilder
 import ru.johnspade.taskobot.Configuration.BotConfig
 import telegramium.bots.high.{Api, BotApi}
 import zio._
+import zio.blocking.Blocking
 import zio.interop.catz._
 
 object TelegramBotApi {
   type TelegramBotApi = Has[Api[Task]]
 
-  val live: URLayer[Has[BotConfig], TelegramBotApi] =
-    ZLayer.fromServiceManaged[BotConfig, Any, Nothing, Api[Task]] { cfg =>
-      ZIO.runtime[Any].toManaged_.flatMap { implicit rts =>
-        BlazeClientBuilder[Task](rts.platform.executor.asEC).resource.toManaged.map { httpClient =>
+  val live: URLayer[Has[BotConfig] with Blocking, TelegramBotApi] =
+    ZLayer.fromServicesManaged[BotConfig, Blocking.Service, Any, Nothing, Api[Task]] { case (cfg, b) =>
+      Task.concurrentEffect.toManaged_.flatMap { implicit CE: ConcurrentEffect[Task] =>
+        BlazeClientBuilder[Task](b.blockingExecutor.asEC).resource.toManaged.map { httpClient =>
           BotApi[Task](httpClient, s"https://api.telegram.org/bot${cfg.token}")
         }
       }
