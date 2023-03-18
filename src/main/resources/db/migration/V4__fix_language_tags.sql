838a057482c39e4b028ab3ee689380bae04d92c5
ilopatin <ivan@ilopatin.ru>
2023-03-18 20:11:37 +0100

    Fix language tags

---
+++ src/main/resources/db/migration/V4__fix_language_tags.sql
@@ -0,0 +1,7 @@
+update users
+    set language = 'zh-TW'
+    where language = 'zh_TW';
+
+update users
+    set language = 'es-ES'
+    where language = 'es_ES';
--- src/main/scala/ru/johnspade/taskobot/messages/Language.scala
+++ src/main/scala/ru/johnspade/taskobot/messages/Language.scala
@@ -2,13 +2,15 @@ package ru.johnspade.taskobot.messages
 
 import doobie.Meta
 
+import java.util.Locale
+
 enum Language(val value: String, val languageName: String):
   case English            extends Language("en", "English")
   case Russian            extends Language("ru", "Русский")
   case Turkish            extends Language("tr", "Turkish")
   case Italian            extends Language("it", "Italian")
-  case TraditionalChinese extends Language("zh_TW", "Traditional Chinese")
-  case Spanish            extends Language("es_ES", "Spanish")
+  case TraditionalChinese extends Language("zh-TW", "Traditional Chinese")
+  case Spanish            extends Language("es-ES", "Spanish")
 
 object Language:
   def withValue(value: String): Option[Language] =
@@ -17,3 +19,5 @@ object Language:
   given Meta[Language] = Meta[String].timap(v =>
     Language.withValue(v).getOrElse(throw new RuntimeException(s"Unknown language: $v"))
   )(_.value)
+
+  extension (language: Language) def toLocale: Locale = Locale.forLanguageTag(language.value)
